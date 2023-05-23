f721bbfc5c6bb2b4ea79c8cfd94ed6fecf9027dc
tomasausino <tomas.ausino@joinmosaic.com>
2023-05-23 10:45:22 -0300

    Adding implementation of HOF methods for MyList class

--- src/main/scala/excercises/MyList.scala
+++ src/main/scala/excercises/MyList.scala
@@ -5,17 +5,30 @@ abstract class MyList[+A] {
   // list of integers
 
   def head: A
+
   def tail: MyList[A]
+
   def isEmpty: Boolean
+
   def add[B >: A](n: B): MyList[B]
+
   def printElements: String
-  override def toString: String ="[" + printElements + "]"
+
+  override def toString: String = "[" + printElements + "]"
 
   def map[B >: A](transformer: A => B): MyList[B]
+
   def filter(condition: A => Boolean): MyList[A]
+
   def flatMap[B](transformer: A => MyList[B]): MyList[B]
+
   // concatenation
   def ++[B >: A](list: MyList[B]): MyList[B]
+
+  def foreach(transformer: A => Unit): Unit
+  def sort(sorter: (A, A) => Int): MyList[A]
+  def zipWith[B >: A](list: MyList[B], zipper: ((A, B) => B)): MyList[B]
+  def fold[B](start: B)(operator: (B, A)=> B): B
 }
 
 case object Empty extends MyList[Nothing] {
@@ -37,6 +50,11 @@ case object Empty extends MyList[Nothing] {
 
   override def ++[B >: Nothing](list: MyList[B]): MyList[B] = list
 
+  override def foreach(transformer: Nothing => Unit): Unit = ()
+  override def sort(sorter: (Nothing, Nothing) => Int): MyList[Nothing] = Empty
+  override def zipWith[B >: Nothing](list: MyList[B], zipper: (Nothing, B) => B): MyList[B] = list
+  override def fold[B](start: B)(operator: (B, Nothing)=> B): B = start
+
 }
 
 case class Cons[+A](h: A, t: MyList[A]) extends MyList[A] {
@@ -46,7 +64,7 @@ case class Cons[+A](h: A, t: MyList[A]) extends MyList[A] {
 
   override def isEmpty: Boolean = false
 
-  override def add[B >: A](n: B): MyList[B] = new Cons(n, this)
+  override def add[B >: A](n: B): MyList[B] = Cons(n, this)
 
   override def printElements: String = {
     if(tail.isEmpty) "" + h
@@ -54,10 +72,10 @@ case class Cons[+A](h: A, t: MyList[A]) extends MyList[A] {
   }
 
   override def map[B >: A](transformer: A => B): MyList[B] =
-    new Cons(transformer(head), tail.map(transformer))
+    Cons(transformer(head), tail.map(transformer))
 
   override def filter(condition: A => Boolean): MyList[A] = {
-    if (condition(head)) new Cons(head, tail.filter(condition))
+    if (condition(head)) Cons(head, tail.filter(condition))
     else tail.filter(condition)
   }
 
@@ -65,6 +83,35 @@ case class Cons[+A](h: A, t: MyList[A]) extends MyList[A] {
     transformer(head) ++ tail.flatMap(transformer)
 
   override def ++[B >: A](list: MyList[B]): MyList[B] = Cons(h, t ++ list)
+
+  override def foreach(transformer: A => Unit): Unit = {
+    if(tail == Empty) transformer(this.h)
+    else {
+      transformer(h)
+      tail.foreach(transformer)
+    }
+  }
+
+  override def sort(sorter: (A, A) => Int): MyList[A] = {
+    def insert(x: A, sortedList: MyList[A]): MyList[A] = {
+      if(sortedList.isEmpty) Cons(x, Empty)
+      else if (sorter(x, sortedList.head) <= 0) Cons(x, sortedList)
+      else Cons(sortedList.head, insert(x, sortedList.tail))
+    }
+
+    val sortedTail = t.sort(sorter)
+    insert(h, sortedTail)
+  }
+
+  override def zipWith[B >: A](list: MyList[B], zipper: (A, B) => B): MyList[B] = {
+      if(list.tail == Empty) Cons(zipper(this.h, list.head), this.t)
+      else Cons(zipper(this.h, list.head), tail.zipWith(list.tail, zipper))
+  }
+
+  override def fold[B](start: B)(operator: (B, A) => B): B = {
+    tail.fold(operator(start, h))(operator)
+  }
+
 }
 
 case class EvenPredicate() extends ((Int) => Boolean) {
@@ -74,13 +121,8 @@ case class IntToStringTransformer() extends ((Int) => String) {
   override def apply(param: Int): String = param.toString
 }
 
-
-
-
-
-
 object ListTest extends App {
-  val listOfInt: MyList[Int] =  Cons(1, Cons(2, Cons(3, Empty)))
+  val listOfInt: MyList[Int] =  Cons(1, Cons(2, Cons(3, Cons(4, Empty))))
   val otherList: MyList[Int] = listOfInt ++ listOfInt
   val listOfStrings: MyList[String] = Empty
 
@@ -93,4 +135,13 @@ object ListTest extends App {
 
   val superAdd = (x: Int) => (y: Int) => x + y
   println(superAdd(3)(4))
+  listOfInt.foreach(println)
+
+  val zipper: (Int, Int) => Int = (x: Int, y: Int) => x * y
+
+
+  println(listOfInt.sort((x, y) => y - x))
+
+
+  println((listOfInt ++ listOfInt).zipWith(listOfInt ++ listOfInt, zipper))
 }
--- src/main/scala/lecture/part3Fp/HOFAndCurries.scala
+++ src/main/scala/lecture/part3Fp/HOFAndCurries.scala
@@ -20,5 +20,28 @@ object HOFAndCurries extends App {
   println(nTimes(plusOne, 10, 1))
   println(myApply(plusOne, 10, 1))
 
+  def nTimesBetter(f: Int => Int, n: Int): (Int => Int) = {
+    if(n <= 0) (x: Int) => x
+    else (x: Int) => nTimesBetter(f, n - 1)(f(x))
+  }
 
+  println(nTimesBetter(plusOne, 10)(1))
+
+  // curried functions
+  val superAdder: Int => (Int => Int) = (x: Int) => (y: Int) => x + y
+  val addThree = superAdder(3)
+  println(addThree(10))
+
+
+  def toCurry[A, B](f: (A, A) => B): (A => A => B) =
+    (x: A) => (y: A) => f(x, y)
+
+  def fromCurry[A, B](f: A => A => B): (A, A) => B =
+    (x: A, y: A) => f(x)(y)
+
+  def compose[A, B, C](f: B => C, g: A => B): A => C =
+    x => f(g(x))
+
+  def andThen[A, B, C](f: A => B, g: B => C): A => C =
+    x => g(f(x))
 }
